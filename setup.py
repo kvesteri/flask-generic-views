658e945c106a4d255446733ad568eda843c3c6d7
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-02-20 17:47:57 +0200

    incremented version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(Command):
 
 setup(
     name='Flask-GenericViews',
-    version='0.1.2',
+    version='0.1.3',
     description='Generic Views for Flask',
     long_description=__doc__,
     author='Konsta Vesterinen',
