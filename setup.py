b0a9a34d4167c460cb4936c3787dd62383c5ef73
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-02-21 14:19:46 +0200

    added integer filters

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(Command):
 
 setup(
     name='Flask-GenericViews',
-    version='0.1.4',
+    version='0.1.5',
     description='Generic Views for Flask',
     long_description=__doc__,
     author='Konsta Vesterinen',
