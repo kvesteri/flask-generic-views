66aa6f20812e3f8e17b92b0cf4a1653c5685c5cf
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-02-24 16:39:17 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(Command):
 
 setup(
     name='Flask-GenericViews',
-    version='0.1.15',
+    version='0.1.16',
     description='Generic Views for Flask',
     long_description=__doc__,
     author='Konsta Vesterinen',
